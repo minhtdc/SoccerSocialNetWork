54393823b350fc5e700f5ecaedda60ffffa220af
Văn Tấn Vũ <68262929+tanvu2804@users.noreply.github.com>
2020-11-13 09:34:58 +0700

    test thêm file

---
+++ app/src/main/java/com/example/soccersocialnetwork/football_field_owner/ZoneInfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.soccersocialnetwork.football_field_owner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.soccersocialnetwork.R;
+
+public class ZoneInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zone_info);
+    }
+}
