d07df5313d44a81babf02b4dcf9b7156cc4cd4b5
Văn Tấn Vũ <68262929+tanvu2804@users.noreply.github.com>
2020-11-26 07:10:14 +0700

    thiết kế giao diện tạo khu

---
+++ app/src/main/java/com/example/soccersocialnetwork/football_field_owner/activity/AddZoneActivity.java
@@ -0,0 +1,16 @@
+package com.example.soccersocialnetwork.football_field_owner.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.soccersocialnetwork.R;
+
+public class AddZoneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_zone);
+    }
+}
