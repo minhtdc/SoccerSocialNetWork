12b8818ce0a0ceb9b516b041e979b700346506d1
Văn Tấn Vũ <68262929+tanvu2804@users.noreply.github.com>
2020-12-05 00:24:57 +0700

    code thêm khu với firebase

--- app/src/main/java/com/example/soccersocialnetwork/football_field_owner/activity/AddZoneActivity.java
+++ app/src/main/java/com/example/soccersocialnetwork/football_field_owner/activity/AddZoneActivity.java
@@ -1,16 +1,247 @@
 package com.example.soccersocialnetwork.football_field_owner.activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.soccersocialnetwork.R;
+import com.example.soccersocialnetwork.football_field_owner.database.DataBaseHelper;
+import com.example.soccersocialnetwork.football_field_owner.model.City;
+import com.example.soccersocialnetwork.football_field_owner.model.Zone;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AddZoneActivity extends AppCompatActivity {
+    private static final String TAG = AddZoneActivity.class.getSimpleName();
+    EditText txtTenKhu, txtDiaChi;
+    Spinner spnTP, spnQuan, spnGioMo, spnPhutMo, spnGioDong, spnPhutDong;
+    ImageView imgAnh;
+    Button btnThem;
+    ArrayList<City> data_tp = new ArrayList<>();
+    ArrayList<String> data_quan = new ArrayList<>();
+    ArrayList<String> data_gio = new ArrayList<>();
+    ArrayList<String> data_phut = new ArrayList<>();
+    ArrayAdapter adapter_tp, adapter_gio, adapter_phut;
+    //    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final int REQUEST_CODE_IMAGE = 999;
+    DatabaseReference mFirebaseDatabase;
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    FirebaseDatabase mFirebaseInstance;
+    String userId;
+    StorageReference storageRef = storage.getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_zone);
+        mFirebaseDatabase = FirebaseDatabase.getInstance().getReference();
+        setControl();
+        setEvent();
+    }
+
+    private void setEvent() {
+        DataBaseHelper dataBaseHelper = new DataBaseHelper(getApplicationContext());
+        dataBaseHelper.createDataBase();
+        dataBaseHelper.openDataBase();
+        KhoiTao();
+        data_tp = dataBaseHelper.getAllCity();
+        setAdapterSpinner(data_tp, adapter_tp, spnTP);
+        setAdapterSpinner(data_gio, adapter_gio, spnGioMo);
+        setAdapterSpinner(data_gio, adapter_phut, spnGioDong);
+        setAdapterSpinner(data_phut, adapter_gio, spnPhutMo);
+        setAdapterSpinner(data_phut, adapter_phut, spnPhutDong);
+        spnTP.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                DataBaseHelper dataBaseHelper = new DataBaseHelper(getApplicationContext());
+                City items = data_tp.get(i);
+                data_quan = dataBaseHelper.getAllDistrict(items.getId());
+                setAdapterSpinner(data_quan, adapter_tp, spnQuan);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        imgAnh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(AddZoneActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_IMAGE);
+
+            }
+        });
+        btnThem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar calendar = Calendar.getInstance();
+                final StorageReference mountainsRef = storageRef.child("image" + calendar.getTimeInMillis() + ".png");
+
+                // Get the data from an ImageView as bytes
+
+                imgAnh.setDrawingCacheEnabled(true);
+                imgAnh.buildDrawingCache();
+                Bitmap bitmap = ((BitmapDrawable) imgAnh.getDrawable()).getBitmap();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                byte[] data = baos.toByteArray();
+
+                UploadTask uploadTask = mountainsRef.putBytes(data);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Toast.makeText(AddZoneActivity.this, "Lỗi!!!", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                        // ...
+                        mountainsRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri downloadPhotoUrl) {
+                                //Now play with downloadPhotoUrl
+                                //Store data into Firebase Realtime Database
+                                createZone(downloadPhotoUrl.toString());
+                                Toast.makeText(AddZoneActivity.this, "thanhcong", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+            }
+        });
+    }
+
+    private byte[] imageViewToByte(ImageView imageView) {
+        Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] bytes = stream.toByteArray();
+        return bytes;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_IMAGE) {
+            if (grantResults.length >= 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_IMAGE);
+            } else {
+                Toast.makeText(this, "Chưa cấp quyền truy cập!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imgAnh.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void createZone(String url) {
+        Zone zone = getZone(url);
+        mFirebaseDatabase.child("Khu").push().setValue(zone);
+    }
+
+    private Zone getZone(String url) {
+        Zone zone = new Zone();
+        zone.setTenKhu(txtTenKhu.getText().toString());
+        zone.setThanhPho(spnTP.getSelectedItem().toString());
+        zone.setQuan(spnQuan.getSelectedItem().toString());
+        zone.setDiaChi(txtDiaChi.getText().toString());
+        zone.setGioMo(spnGioMo.getSelectedItem().toString());
+        zone.setGioDong(spnPhutMo.getSelectedItem().toString());
+        zone.setPhutMo(spnGioDong.getSelectedItem().toString());
+        zone.setPhutDong(spnPhutDong.getSelectedItem().toString());
+        zone.setAnh(url);
+        return zone;
+    }
+
+    private void setAdapterSpinner(ArrayList data, ArrayAdapter adapter, Spinner spinner) {
+        if (adapter == null) {
+            adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, data);
+            spinner.setAdapter(adapter);
+            adapter.notifyDataSetChanged();
+        } else {
+            adapter.notifyDataSetChanged();
+            spinner.setSelection(adapter.getCount() - 1);
+        }
+    }
+
+    private void setControl() {
+        txtTenKhu = findViewById(R.id.txtTenKhu);
+        txtDiaChi = findViewById(R.id.txtDiaChi);
+        spnTP = findViewById(R.id.spnTP);
+        spnQuan = findViewById(R.id.spnQuan);
+        spnGioMo = findViewById(R.id.spnGioMo);
+        spnPhutMo = findViewById(R.id.spnPhutMo);
+        spnGioDong = findViewById(R.id.spnGioDong);
+        spnPhutDong = findViewById(R.id.spnPhutDong);
+        imgAnh = findViewById(R.id.imgAnhKhu);
+        btnThem = findViewById(R.id.btnThem);
+
+    }
+
+    void KhoiTao() {
+        String a,b;
+        for (int i = 1; i < 25; i++) {
+            if(i<10){
+                a = String.valueOf(i);
+                b = "0" + a;
+            }else {
+                b = String.valueOf(i);
+            }
+            data_gio.add(b);
+        }
+        data_phut.add("00");
+        data_phut.add("30");
     }
 }
---
+++ app/src/main/java/com/example/soccersocialnetwork/football_field_owner/database/DataBaseHelper.java
@@ -0,0 +1,149 @@
+package com.example.soccersocialnetwork.football_field_owner.database;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.soccersocialnetwork.football_field_owner.model.City;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+    private static String TAG = "DataBaseHelper"; // Tag just for the LogCat window
+    //destination path (location) of our database on device
+    private static String DB_PATH = "";
+    private static String DB_NAME = "dvhcvn.db";// Database name
+    private SQLiteDatabase mDataBase;
+    private final Context mContext;
+
+
+    private String AV_TABLE = "district";
+
+    public String ID = "id";
+    public String NAME = "name";
+    public String TYPE = "type";
+
+
+    // do đường dẫn ở phiên bản API > 17 thay đổi nên chúng ta cần kiểm tra nhé
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);// 1? Its database Version
+        if (android.os.Build.VERSION.SDK_INT >= 17) {
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        } else {
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        }
+        this.mContext = context;
+    }
+
+    public void createDataBase() {
+        //If the database does not exist, copy it from the assets.
+
+        boolean mDataBaseExist = checkDataBase();
+        if (!mDataBaseExist) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                //Copy the database from assests
+                copyDataBase();
+                Log.e(TAG, "createDatabase database created");
+            } catch (IOException mIOException) {
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+    }
+
+    //Check that the database exists here: /data/data/your package/databases/Da Name
+    private boolean checkDataBase() {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        //Log.v("dbFile", dbFile + "   "+ dbFile.exists());
+        return dbFile.exists();
+    }
+
+    //Copy the database from assets
+    private void copyDataBase() throws IOException {
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream mOutput = new FileOutputStream(outFileName);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer)) > 0) {
+            mOutput.write(mBuffer, 0, mLength);
+        }
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
+    }
+
+    //Open the database, so we can query it
+    public boolean openDataBase() throws SQLException {
+        String mPath = DB_PATH + DB_NAME;
+        //Log.v("mPath", mPath);
+        mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        //mDataBase = SQLiteDatabase.openDatabase(mPath, null, SQLiteDatabase.NO_LOCALIZED_COLLATORS);
+        return mDataBase != null;
+    }
+
+    @Override
+    public synchronized void close() {
+        if (mDataBase != null)
+            mDataBase.close();
+        super.close();
+    }
+
+    public ArrayList<City> getAllCity() {
+        ArrayList<City> cities = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        String SQL = "SELECT * FROM province";
+
+        Cursor cursor = sqLiteDatabase.rawQuery(SQL, null);
+        cursor.moveToFirst();
+        do {
+            City city = new City();
+            city.setId(cursor.getInt(0));
+            city.setName(cursor.getString(1));
+            city.setType(cursor.getString(2));
+            cities.add(city);
+        } while (cursor.moveToNext());
+
+
+        return cities;
+    }
+    public ArrayList<String> getAllDistrict(int key) {
+        ArrayList<String> district = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+
+        String SQL = "SELECT * FROM district where province_id = '" + key +"'";
+
+        Cursor cursor = sqLiteDatabase.rawQuery(SQL, null);
+        cursor.moveToFirst();
+        try {
+            do {
+                String dis = cursor.getString(1);
+                district.add(dis);
+            } while (cursor.moveToNext());
+
+        }catch (Exception ex){
+
+        }
+        return district;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
