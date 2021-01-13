5f2a350c52b1246b13222cfb354e241b0206f87e
Văn Tấn Vũ <68262929+tanvu2804@users.noreply.github.com>
2021-01-13 17:45:12 +0700

    admin

---
+++ app/src/main/java/com/example/soccersocialnetwork/Admin/Adapter/AdapterUser.java
@@ -0,0 +1,5 @@
+package com.example.soccersocialnetwork.Admin.Adapter;
+
+public class AdapterUser {
+
+}
---
+++ app/src/main/java/com/example/soccersocialnetwork/Admin/Adapter/Adapter_TestCLickTeam.java
@@ -0,0 +1,166 @@
+package com.example.soccersocialnetwork.Admin.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.soccersocialnetwork.DoanThanhTung.Models.Team;
+import com.example.soccersocialnetwork.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class Adapter_TestCLickTeam extends BaseAdapter implements Filterable {
+    Context context;
+    ArrayList<Team> data;
+    ArrayList<Team> datafull;
+
+    ValueFilter valueFilter;
+    int resource;
+    DatabaseReference mDatabase;
+
+    public Adapter_TestCLickTeam(@NonNull Context context, int resource, ArrayList<Team> data) {
+
+        this.context = context;
+        this.data = data;
+        this.resource = resource;
+        this.datafull = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    static class Holder {
+        ImageView imageView, imgXoa;
+        TextView txtTenDoi, txtKhuVuc, txtThongtinGioiHieu, txtThongTinSL_CauThu_lstTeam;
+
+    }
+
+    int soCauThu = 0;
+    ValueEventListener valueEventListener;
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        Holder holder = new Holder();
+        if (view == null) {
+            holder = new Holder();
+            view = LayoutInflater.from(context).inflate(resource, null);
+
+            holder.imageView = view.findViewById(R.id.imgLogo_Teams_list_teams);
+            holder.txtTenDoi = view.findViewById(R.id.txtTendoi_list_teams);
+            holder.txtKhuVuc = view.findViewById(R.id.txtThongTinKhuVuc_lstTeams);
+            holder.txtThongtinGioiHieu = view.findViewById(R.id.txtThongTinGioiThieu_lstTeams);
+            holder.txtThongTinSL_CauThu_lstTeam = view.findViewById(R.id.txtThongTinSL_CauThu_lstTeam);
+            holder.imgXoa = view.findViewById(R.id.imgXoa);
+
+            view.setTag(holder);
+        } else
+            holder = (Holder) view.getTag();
+
+        final Team team = data.get(position);
+
+
+        //   readuser(team.getIdDoi() + "");
+        Picasso.get().load(team.getHinhAnh()).into(holder.imageView);
+        holder.txtTenDoi.setText(team.getTenDoi());
+        holder.txtKhuVuc.setText(team.getKhuVuc());
+        holder.txtThongtinGioiHieu.setText(team.getGioiThieu());
+        holder.imgXoa.setImageResource(R.drawable.icon_delete);
+        final DatabaseReference databaseReference1 = FirebaseDatabase.getInstance().getReference();
+        final Holder finalHolder = holder;
+
+        valueEventListener = databaseReference1.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                soCauThu = 0;
+                for (DataSnapshot dt :
+                        snapshot.getChildren()) {
+                    for (DataSnapshot dtt :
+                            dt.child("listDoi").getChildren()) {
+                        if (dtt.getKey().equals(team.getIdDoi() + "")) {
+                            soCauThu++;
+                        }
+                    }
+                }
+                finalHolder.txtThongTinSL_CauThu_lstTeam.setText(soCauThu+"");
+                databaseReference1.removeEventListener(valueEventListener);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+
+        if (valueFilter == null) {
+            valueFilter = new ValueFilter();
+        }
+        return valueFilter;
+    }
+
+    //---------search
+    private class ValueFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<Team> strings = new ArrayList<>();
+            if (constraint.toString().isEmpty()) {
+                strings.addAll(datafull);
+            } else {
+                for (Team movie : datafull) {
+                    if (movie.getTenDoi().toLowerCase().contains(constraint.toString().toLowerCase().trim())) {
+                        strings.add(movie);
+                    }
+                }
+            }
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = strings;
+            return filterResults;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            data = (ArrayList<Team>) results.values;
+
+            notifyDataSetChanged();
+        }
+    }
+
+
+
+}
